cdadcd0a07799d2644ec6616b03b205ca4bb68e6
idpass1708 <65698106+idpass1708@users.noreply.github.com>
2025-03-16 21:25:54 +0700

    bash.sh

---
+++ bash.sh
@@ -0,0 +1,7 @@
+/taqri-kehadiran  
+│── index.html      # Front-end utama  
+│── style.css       # Tampilan CSS  
+│── script.js       # Logika JavaScript  
+│── server.php      # Back-end untuk menyimpan data  
+│── data.json       # Penyimpanan data kehadiran  
+└── README.md       # Dokumentasi proyek 
